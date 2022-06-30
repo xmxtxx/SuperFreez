a1ee2733099897759c28f16d73a7b5f535975eb8
xmxtxx <heinrich.mathieu@gmx.ch>
2022-06-30 10:50:34 +0200

    Update conection.php

--- php/conection.php
+++ php/conection.php
@@ -1,1 +1,18 @@
+<?php 
+function OpenCon(){
+    $host ="localhost";
+    $database="";
+    $user="";
+    $password="";
 
+
+    $connection = new mysqli($host,$user,$password,$database) or die ("Datenbankverbindung Fehlgeschlagen");
+
+    return $connection;
+}
+
+function CloseCon($con){
+    $con -> close();
+}
+
+?>
