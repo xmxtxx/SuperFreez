410ab3ce76683e436678e690687d2c04bafb0ca5
xmxtxx <heinrich.mathieu@gmx.ch>
2022-06-30 10:49:10 +0200

    Index&css
    Erste anpassungen index & Css

--- index.php
+++ index.php
@@ -1,16 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SuperFreeze</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
-    
-    <h1>Mathieu</h1>
-    <h1>Cédric</h1>
-    <h1>Nattu</h1>
-    <h1>Andres</h1>
+
+    <h1>Meine SuperFreezer</h1>
+
+
+    <!--Implementation Php!-->
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th> Infos </th>
+
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td><table>
+                <tr>
+                    <td>Anzahl Fächer: </td>
+                    <td>5</td>
+                </tr>
+                
+                <tr>
+                    <td>Inventar: </td>
+                    <td>95%</td>
+                </tr>
+                <tr>
+                    <td>Staus: </td>
+                    <td>Operabel</td>
+                </tr>
+                <tr>
+                    <td><a href="#" class= "option">SuperFreezer verwalten</a></td>
+                </tr>
+            </table>
+            </td>
+        </tr>
+
+    </table>
+
+
+
+
 </body>
+
 </html>
